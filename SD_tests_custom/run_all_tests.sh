183e728127963e37c94909a939bf203f293f14f6
Florrdv <flor@ronsmans.me>
2022-10-09 19:05:32 +0200

    Reworked file names

--- SD_tests_custom/run_all_tests.sh
+++ SD_tests_custom/run_all_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo find . -name "*.txt" -exec python3 test.py -s -i {} -c ../SD_Group07.circ --logisimpath=../common/logisim-generic-2.7.1.jar \;
+sudo find . -name "*.txt" -exec python3 test.py -s -i {} -c ../datapath.circ --logisimpath=../common/logisim-generic-2.7.1.jar \;
